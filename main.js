6f89ed234a83098dfac361cf9a145ec597f8dad2
kim <jayfulkr@gmail.com>
2023-07-11 17:41:26 +0900

    start

---
+++ main.js
@@ -0,0 +1,7 @@
+// const title = document.querySelector('#title');
+
+// window.addEventListener('scroll', () => {
+//   if (`${scrollY / 10 + 1}` < 60) {
+//     title.style.transform = `scale(${scrollY / 10 + 1})`;
+//   }
+// });
